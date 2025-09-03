5c9b419074beee39b17d9a9e7519168e01fd62ab
Emanuela <emanuelavon@gmx.de>
2025-09-03 09:51:54 +0200

    bug fix

--- index.php
+++ index.php
@@ -7,18 +7,17 @@
 
     <title>Document</title>
 </head>
+
 <body>
     <!--navbar -->
     <section>
         <nav class="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-4 text-2xl">
-           <div class="flex space-x-6 hover:[&>li]:text-red-500 hover[&>li]:underline">
-            <ul class="flex space-x-4 p-4">
-                <li><a href="#">home</a></li>
-                <li><a href="#">about</a></li>
-                <li><a href="#">portfolio</a></li>
-                <li><a href="#">contact</a></li>
+            <ul class="flex space-x-6 p-4 hover:[&>li]:underline">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
-           </div>
         </nav>
     </section>
 
@@ -34,37 +33,57 @@
     </div>
 </section>
  
+
+    <!--code area section -->
+<section>
+    <div class="min-h-32 bg-red-100">
+     <?php
+        $my_name = "Prof. Lee";
+        $welcome = "Welcome to my website";
+        echo $welcome.$my_name;
+     ?>
+     <span class="text-red-500"><?=$my_name?></span>
+    </div>
+</section>
+
+
   <!--section content-->
   <section>
     <div class="bg-violet-200 p-8">
-        <div class="grid grid-cols-3 space-x-4">
+        <div class="grid grid-cols-3 gap-4">
+
             <div class="border border-orange-500 rounded-md shadow-lg p-4">
                 <div class="bg-gray-300 text-center text-lg font-semibold pt-2 pb-2 rounded-tl-md rounded-tr-md ">header</div>
                 <div class="p-4 border b">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
                 <div class="p-4">read more </div>
-                bla bla
-            </div>         
-        </div>
-        ´<div class="p-8 bg-gray-700">
-            <div class="text-gray-100 text 2xl mb-8">contact me</div>
-            <form methode="post" class="grid space-y-8">
-                <input name="visitor Name" class="rounded p-2 txt-lg w-72 type="text" placeholder="Please write your name">
-                <textarea name="visitor Massage" class="rounded p-2 text-lg w72" placeholder="please write your message"></textarea>
-                <button type="submit" class="bg blue-500 text-gray-100 w-32 rounded p-2 text-lg">senden</button>
-            </form>
-        </div>
-       
+            </div>
+            <div class="border border-orange-500 rounded-md shadow-lg p-4">
+                <div class="bg-gray-300 text-center text-lg font-semibold pt-2 pb-2 rounded-tl-md rounded-tr-md ">header</div>
+                <div class="p-4 border b">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+                <div class="p-4">read more </div>
+            </div>
+            <div class="border border-orange-500 rounded-md shadow-lg p-4">
+                <div class="bg-gray-300 text-center text-lg font-semibold pt-2 pb-2 rounded-tl-md rounded-tr-md ">header</div>
+                <div class="p-4 border b">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+                <div class="p-4">read more </div>
+            </div>
+        </div>       
     </div>
   </section>
  
   <!--section footer-->
   <section>
-      <footer>
-            <div class="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-4 text-xs">
-              <?php  echo "copyright 2025 @ Prof. Lee" ?>
-            </div>
+        <div class="p-8 bg-gray-700">
+      <div class="text-gray-100 text-2xl mb-8">contact me</div>
+      <form class="grid space-y-8" action="/index.html" method="post">
+        <input  name="visitor_name" class="rounded p-2 text-lg w-72" type="text" placeholder="please write your name">
+        <textarea name="visitor_message" class="rounded p-2 text-lg w-72" placeholder="please write your message"></textarea>
+        <button type="submit" class="bg-blue-500 text-gray-100 w-32 rounded p-2 text-lg" >senden</button>
+      </form>
+    </div>
+      <footer class="bg-gray-800 text-gray-100 p-4 h-32">   
+         <?php  echo "copyright Prof Lee 2025" ?>
       </footer>
   </section>
 </body>
 </html>
- 
