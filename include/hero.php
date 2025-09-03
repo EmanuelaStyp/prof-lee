80625c952c1275cd100977a703c23f3877708a18
Emanuela <emanuelavon@gmx.de>
2025-09-03 11:46:48 +0200

    functional component

--- include/hero.php
+++ include/hero.php
@@ -1,12 +1,13 @@
     <!-- section Hero-->
 <section>
-    <div class="bg-gradient-to-tr from-cyan-200 via-blue-400 to-indigo-600  text-gray-100 min-h-32 shadow-md text-center p-4 space" >
+    <div class="bg-gradient-to-tr from-cyan-200 via-blue-400 to-indigo-600  text-gray-100 min-h-26
+     shadow-md text-center p-2 space" >
         <div>
             <img class="rounded-full shadow-lg shadow-cyan-500 "src="./assets/img/sunset.webp">
         </div>
        <div>
-            div class="text-8xl">Welcome Prof. Lee Website</div>
-            <p class="text-4xl"> PHD Microprocessor Design</p>
+            <div class="text-4xl">Welcome Prof. Lee Website</div>
+            <p class="text-2xl"> PHD Microprocessor Design</p>
         </div>
     </div>
 </section>
